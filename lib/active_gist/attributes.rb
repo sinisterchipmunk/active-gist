964d9a3b862bede5c898616175116c80fd1db5a0
Levi Brown <levi.brown@g5platform.com>
2013-07-02 09:47:26 -0700

    add commits_url and forks_url | add attr_reader

--- lib/active_gist/attributes.rb
+++ lib/active_gist/attributes.rb
@@ -1,92 +1,53 @@
 module ActiveGist::Attributes
   GIST_ATTRIBUTES = %w(url id description public user files comments
                        html_url git_pull_url git_push_url created_at
-                       forks history updated_at comments_url)
-                       
+                       forks forks_url history updated_at comments_url commits_url)
+
   def self.included(base) #:nodoc:
     base.define_attribute_methods GIST_ATTRIBUTES
   end
-  
-  def forks
-    @forks
-  end
-  
-  def history
-    @history
-  end
-  
-  def url
-    @url
-  end
-  
-  def id
-    @id
-  end
-  
-  def description
-    @description
-  end
-  
+
+  attr_reader :url,
+              :id,
+              :description,
+              :user,
+              :files,
+              :comments,
+              :comments_url,
+              :created_at,
+              :forks,
+              :forks_url,
+              :history,
+              :updated_at,
+              :html_url,
+              :git_pull_url,
+              :git_push_url,
+              :commits_url
+
   def public?
     !!@public
   end
   alias public public?
-  
-  def user
-    @user
-  end
-  
-  def files
-    @files
-  end
-  
-  def comments
-    @comments
-  end
-  
-  def html_url
-    @html_url
-  end
-  
-  def git_pull_url
-    @git_pull_url
-  end
-  
-  def git_push_url
-    @git_push_url
-  end
-  
-  def created_at
-    @created_at
-  end
-  
-  def updated_at
-    @updated_at
-  end
 
-  def comments_url
-    @comments_url
-  end
-  
   def description=(descr)
     description_will_change!
     @description = descr
   end
-  
+
   def public=(pub)
     public_will_change!
     @public = pub
   end
-  
+
   def files=(files)
     files_will_change!
     @files = files
   end
-  
+
   def attributes
     GIST_ATTRIBUTES.inject({}) { |h,k| h[k] = self[k]; h }
   end
-  
+
   def attributes=(attributes)
     attributes.each do |key, value|
       if respond_to?(:"#{key}=")
@@ -104,7 +65,7 @@ module ActiveGist::Attributes
   def [](attribute)
     send attribute.to_sym
   end
-  
+
   def []=(attribute, value)
     send :"#{attribute}=", value
   end
